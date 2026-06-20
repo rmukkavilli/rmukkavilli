d2088a1c7bae2e50d6a383afda7669af42f4d9f0
rmukkavilli <ravirameshm@gmail.com>
2026-06-20 16:57:24 -0700

    testing

--- playwright-demo/conftest.py
+++ playwright-demo/conftest.py
@@ -4,6 +4,10 @@ from playwright.sync_api import sync_playwright
 
 @pytest.fixture(scope="session")
 def browser():
+    is_ci = os.getenv('CI', "false").tolower() = "true"
     with sync_playwright() as p:
-        browser = p.chromium.launch(channel="chrome", headless=False)
+        if is_ci:
+            browser = p.chromium.launch(headless=True)
+        else:
+            browser = p.chromium.launch(channel="chrome", headless=False)
         yield browser
