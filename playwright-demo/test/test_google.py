9d27639367aa92914b6ed7728086d39479cc9935
rmukkavilli <ravirameshm@gmail.com>
2026-06-20 16:25:29 -0700

    workflows

--- playwright-demo/test/test_google.py
+++ playwright-demo/test/test_google.py
@@ -1,6 +1,8 @@
 def test_google(browser):
     page = browser.new_page()
     page.goto("https://www.google.com")
+    print(page.title())
+    assert "Google" in page.title()
     page.locator("[name=q]").fill("hello")
     page.keyboard.press("Enter")
     page.wait_for_timeout(10000)
